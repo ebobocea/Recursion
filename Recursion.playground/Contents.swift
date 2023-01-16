b0d927d105f1b15efb77a25761f245df3876cbad
Elisei Bobocea <elisey93@icloud.com>
2023-01-16 15:16:34 +0000

    power added

--- Recursion.playground/Contents.swift
+++ Recursion.playground/Contents.swift
@@ -7,6 +7,20 @@ func factorial(of number: Int) -> Int {
     return number * factorial(of: number - 1)
 }
 
+func pow(of power: Int, number: Int)->Int{
+    //base case
+    if power == 0 {
+        return 1
+    }
+    //recursiv case
+    else{
+        return number * pow(of: power - 1, number: number)
+    }
+}
+
+
 let result = factorial(of: 5)
-print(result) // Output: 120
+print(result)
+
+print(pow(of: 3, number: 2))
 
